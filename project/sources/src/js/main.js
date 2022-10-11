97d86537fdfe46d1019d314f89d3c23ef420a716
Vasily Ilyuhin <v.ilyuhin@gmail.com>
2022-10-11 15:47:17 +0300

    feat: about block animation completed

--- project/sources/src/js/main.js
+++ project/sources/src/js/main.js
@@ -1,15 +1,28 @@
 document.addEventListener("DOMContentLoaded", function (event) {
-  const tl = gsap.timeline();
-  tl.fromTo(".about__container", { x: "0", y: "0" }, { x: "-105%", y: "0" });
-
+  const first = document.querySelector(".first");
   const aboutBlock = document.querySelector(".about");
-  ScrollTrigger.create({
-    animation: tl,
-    trigger: ".about",
-    start: "top top",
-    end: () => aboutBlock.offsetWidth * 2,
-    scrub: true,
-    pin: true,
-    markers: true,
+
+  // move about block
+  gsap.registerPlugin(ScrollTrigger);
+
+  gsap.to(".about__container", {
+    x: "-105%",
+    scrollTrigger: {
+      trigger: ".about__container",
+      start: "top top",
+      end: () => aboutBlock.offsetWidth * 2,
+      scrub: true,
+      pin: true,
+    },
+  });
+  // move blue figure
+  gsap.to(".svg-image-pathnew", {
+    scrollTrigger: {
+      trigger: ".svg-image-pathnew",
+      start: () => first.offsetHeight * 2,
+      end: () => first.offsetHeight * 4,
+      scrub: true,
+      pin: true,
+    },
   });
 });
