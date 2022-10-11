b6618a237b489f16dbc351d409713940c7f5f2ec
Vasily Ilyuhin <v.ilyuhin@gmail.com>
2022-10-11 10:21:29 +0300

    feat: add horisontal move by scroll to about block

--- project/sources/src/js/main.js
+++ project/sources/src/js/main.js
@@ -1,19 +1,15 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    // Your code here
-    
-    console.log('It works!');
-    fun('It also works!');
-    console.log(es6Func('JS'));
-    console.log(es6Func('JS', 'Popular Language'));
+document.addEventListener("DOMContentLoaded", function (event) {
+  const tl = gsap.timeline();
+  tl.fromTo(".about__container", { x: "0", y: "0" }, { x: "-105%", y: "0" });
+
+  const aboutBlock = document.querySelector(".about");
+  ScrollTrigger.create({
+    animation: tl,
+    trigger: ".about",
+    start: "top top",
+    end: () => aboutBlock.offsetWidth * 2,
+    scrub: true,
+    pin: true,
+    markers: true,
+  });
 });
-
-// Your functions here
-function fun(params) {
-    console.log('fun says: ' + params);
-}
-
-// Let's write on ES6!
-const es6Func = (param1, param2 = 'ES6') => {
-    let words = param1 +' — '+ param2;
-    return `This is ${param1} and ${param2}, together: ${words}`;
-}
