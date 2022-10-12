b4ae3ad54bad5717bf9b55e1960c0eef388806bf
Vasil Ilyuhin <v.ilyuhin@gmail.com>
2022-10-12 23:59:29 +0300

    feat: rewrite js-code to use magicscroll library

--- project/html/js/main.js
+++ project/html/js/main.js
@@ -1,28 +1,102 @@
-document.addEventListener("DOMContentLoaded", function (event) {
-  const first = document.querySelector(".first");
-  const aboutBlock = document.querySelector(".about");
+document.addEventListener('DOMContentLoaded', function (event) {
+  let controller = new ScrollMagic.Controller();
 
   // move about block
-  gsap.registerPlugin(ScrollTrigger);
+  let moveAboutBlock = gsap.timeline().to('.about__container', { x: '-105%' });
 
-  gsap.to(".about__container", {
-    x: "-105%",
-    scrollTrigger: {
-      trigger: ".about__container",
-      start: "top top",
-      end: () => aboutBlock.offsetWidth * 2,
-      scrub: true,
-      pin: true,
-    },
-  });
+  new ScrollMagic.Scene({
+    triggerElement: '.about__container',
+    triggerHook: 'onLeave',
+    duration: '200%',
+  })
+    .setPin('.about__container')
+    .setTween(moveAboutBlock)
+    .addTo(controller);
+
   // move blue figure
-  gsap.to(".svg-image-pathnew", {
-    scrollTrigger: {
-      trigger: ".svg-image-pathnew",
-      start: () => first.offsetHeight * 2,
-      end: () => first.offsetHeight * 4,
-      scrub: true,
-      pin: true,
-    },
-  });
+  new ScrollMagic.Scene({
+    triggerElement: '.svg-image-pathnew',
+    triggerHook: 'onLeave',
+    offset: '800%',
+    duration: '400%',
+  })
+    .setPin('.svg-image-pathnew')
+    // .addIndicators()
+    .addTo(controller);
+
+  // change scale of blue figure
+  const TL_changeScale = gsap
+    .timeline()
+    .fromTo('.svg-image-pathnew', { scale: 1 }, { scale: 20 });
+
+  new ScrollMagic.Scene({
+    triggerElement: '.build',
+    triggerHook: 'onLeave',
+    offset: '250%',
+    duration: '25%',
+  })
+    .setPin('.svg-image-pathnew')
+    .setTween(TL_changeScale)
+    // .addIndicators()
+    .addTo(controller);
+
+  // change opacity of blue figure
+  const TL_changeFillOpacity = gsap
+    .timeline()
+    .fromTo('.blue__path', { fillOpacity: 0.06 }, { fillOpacity: 1 });
+
+  new ScrollMagic.Scene({
+    triggerElement: '.build',
+    triggerHook: 'onLeave',
+    offset: '250%',
+    duration: '25%',
+  })
+    .setPin('.svg-image-pathnew')
+    .setTween(TL_changeFillOpacity)
+    // .addIndicators()
+    .addTo(controller);
+
+  // fix the build block
+  new ScrollMagic.Scene({
+    triggerElement: '.build',
+    triggerHook: 'onLeave',
+    duration: '110%',
+  })
+    .setPin('.build', { pushFollowers: true })
+    // .addIndicators()
+    .addTo(controller);
+
+  // build__title color
+  const TL_titleColorChange = gsap
+    .timeline()
+    .fromTo(
+      '.build__title',
+      { color: '#0c2742', scale: 1.56, translateX: '-52%', translateY: '125%' },
+      { color: '#fff', scale: 1, translateX: '0', translateY: '0' }
+    );
+
+  new ScrollMagic.Scene({
+    triggerElement: '.build',
+    triggerHook: 'onLeave',
+    offset: '250%',
+    duration: '25%',
+  })
+    .setTween(TL_titleColorChange)
+    // .addIndicators()
+    .addTo(controller);
+
+  // change opacity of block info
+  const TL_changeBuildOpacity = gsap
+    .timeline()
+    .fromTo('.build__hidden', { opacity: 0 }, { opacity: 1 });
+
+  new ScrollMagic.Scene({
+    triggerElement: '.build',
+    triggerHook: 'onLeave',
+    offset: '600%',
+    duration: '40%',
+  })
+    .setTween(TL_changeBuildOpacity)
+    // .addIndicators()
+    .addTo(controller);
 });
