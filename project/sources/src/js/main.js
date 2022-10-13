2d637f516f959e5a34686bd644f95f452cdc8781
Vasily Ilyuhin <v.ilyuhin@gmail.com>
2022-10-13 17:26:54 +0300

    feat: add unfinished testimonials

--- project/sources/src/js/main.js
+++ project/sources/src/js/main.js
@@ -1,41 +1,52 @@
-document.addEventListener('DOMContentLoaded', function (event) {
+document.addEventListener("DOMContentLoaded", function (event) {
   let controller = new ScrollMagic.Controller();
 
+  // fix header by scroll
+  new ScrollMagic.Scene({
+    triggerElement: ".first",
+    triggerHook: "onLeave",
+    offset: "20px",
+    duration: 0,
+  })
+    .setClassToggle(".first", "fixed")
+    // .addIndicators()
+    .addTo(controller);
+
   // move about block
-  let moveAboutBlock = gsap.timeline().to('.about__container', { x: '-105%' });
+  let moveAboutBlock = gsap.timeline().to(".about__container", { x: "-105%" });
 
   new ScrollMagic.Scene({
-    triggerElement: '.about__container',
-    triggerHook: 'onLeave',
-    duration: '200%',
+    triggerElement: ".about__container",
+    triggerHook: "onLeave",
+    duration: "200%",
   })
-    .setPin('.about__container')
+    .setPin(".about__container")
     .setTween(moveAboutBlock)
     .addTo(controller);
 
   // move blue figure
   new ScrollMagic.Scene({
-    triggerElement: '.svg-image-pathnew',
-    triggerHook: 'onLeave',
-    offset: '800%',
-    duration: '400%',
+    triggerElement: ".svg-image-pathnew",
+    triggerHook: "onLeave",
+    offset: "800%",
+    duration: "400%",
   })
-    .setPin('.svg-image-pathnew')
+    .setPin(".svg-image-pathnew")
     // .addIndicators()
     .addTo(controller);
 
   // change scale of blue figure
   const TL_changeScale = gsap
     .timeline()
-    .fromTo('.svg-image-pathnew', { scale: 1 }, { scale: 20 });
+    .fromTo(".svg-image-pathnew", { scale: 1 }, { scale: 20 });
 
   new ScrollMagic.Scene({
-    triggerElement: '.build',
-    triggerHook: 'onLeave',
-    offset: '250%',
-    duration: '25%',
+    triggerElement: ".build",
+    triggerHook: "onLeave",
+    offset: "250%",
+    duration: "25%",
   })
-    .setPin('.svg-image-pathnew')
+    .setPin(".svg-image-pathnew")
     .setTween(TL_changeScale)
     // .addIndicators()
     .addTo(controller);
@@ -43,26 +54,26 @@ document.addEventListener('DOMContentLoaded', function (event) {
   // change opacity of blue figure
   const TL_changeFillOpacity = gsap
     .timeline()
-    .fromTo('.blue__path', { fillOpacity: 0.06 }, { fillOpacity: 1 });
+    .fromTo(".blue__path", { fillOpacity: 0.06 }, { fillOpacity: 1 });
 
   new ScrollMagic.Scene({
-    triggerElement: '.build',
-    triggerHook: 'onLeave',
-    offset: '250%',
-    duration: '25%',
+    triggerElement: ".build",
+    triggerHook: "onLeave",
+    offset: "250%",
+    duration: "25%",
   })
-    .setPin('.svg-image-pathnew')
+    .setPin(".svg-image-pathnew")
     .setTween(TL_changeFillOpacity)
     // .addIndicators()
     .addTo(controller);
 
   // fix the build block
   new ScrollMagic.Scene({
-    triggerElement: '.build',
-    triggerHook: 'onLeave',
-    duration: '110%',
+    triggerElement: ".build",
+    triggerHook: "onLeave",
+    duration: "150%",
   })
-    .setPin('.build', { pushFollowers: true })
+    .setPin(".build", { pushFollowers: true })
     // .addIndicators()
     .addTo(controller);
 
@@ -70,16 +81,16 @@ document.addEventListener('DOMContentLoaded', function (event) {
   const TL_titleColorChange = gsap
     .timeline()
     .fromTo(
-      '.build__title',
-      { color: '#0c2742', scale: 1.56, translateX: '-52%', translateY: '125%' },
-      { color: '#fff', scale: 1, translateX: '0', translateY: '0' }
+      ".build__title",
+      { color: "#0c2742", scale: 1.56, translateX: "-52%", translateY: "125%" },
+      { color: "#fff", scale: 1, translateX: "0", translateY: "0" }
     );
 
   new ScrollMagic.Scene({
-    triggerElement: '.build',
-    triggerHook: 'onLeave',
-    offset: '250%',
-    duration: '25%',
+    triggerElement: ".build",
+    triggerHook: "onLeave",
+    offset: "250%",
+    duration: "25%",
   })
     .setTween(TL_titleColorChange)
     // .addIndicators()
@@ -88,15 +99,101 @@ document.addEventListener('DOMContentLoaded', function (event) {
   // change opacity of block info
   const TL_changeBuildOpacity = gsap
     .timeline()
-    .fromTo('.build__hidden', { opacity: 0 }, { opacity: 1 });
+    .fromTo(".build__hidden", { opacity: 0 }, { opacity: 1 });
 
   new ScrollMagic.Scene({
-    triggerElement: '.build',
-    triggerHook: 'onLeave',
-    offset: '600%',
-    duration: '40%',
+    triggerElement: ".build",
+    triggerHook: "onLeave",
+    offset: "600%",
+    duration: "40%",
   })
     .setTween(TL_changeBuildOpacity)
     // .addIndicators()
     .addTo(controller);
+
+  // change opacity of block info
+  const TL_translateBuildMore = gsap
+    .timeline()
+    .fromTo(".build__more", { opacity: 0, y: "50px" }, { opacity: 1, y: "0" });
+
+  new ScrollMagic.Scene({
+    triggerElement: ".build",
+    triggerHook: "onLeave",
+    offset: "650%",
+    duration: "10%",
+  })
+    .setTween(TL_translateBuildMore)
+    .addTo(controller);
+
+  // .news-pathnew translate
+  const TL_newFigureTransform = gsap
+    .timeline()
+    .fromTo(".news-pathnew", { translateY: "40%" }, { translateY: "-10%" });
+
+  new ScrollMagic.Scene({
+    triggerElement: ".news",
+    triggerHook: "onLeave",
+    duration: "80%",
+  })
+    .setTween(TL_newFigureTransform)
+    .addIndicators()
+    .addTo(controller);
+
+  // .news-pathnew change color
+  const TL_newPathChangeColor = gsap
+    .timeline()
+    .fromTo(
+      ".news__path",
+      { fill: "#1B75BC", fillOpacity: "0.06" },
+      { fill: "#02AD23", fillOpacity: "1" }
+    );
+
+  new ScrollMagic.Scene({
+    triggerElement: ".news",
+    triggerHook: "onLeave",
+    duration: "80%",
+  })
+    .setTween(TL_newPathChangeColor)
+    .addIndicators()
+    .addTo(controller);
+
+  // Slider in news block
+  const newsSwiper = new Swiper(".news__slider", {
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 50,
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+      },
+      // when window width is >= 640px
+      640: {
+        slidesPerView: 3,
+        spaceBetween: 40,
+      },
+    },
+  });
+
+  document
+    .querySelector(".testimonials__prev")
+    .addEventListener("click", () => {
+      console.log("prev");
+    });
+
+  // Slider in testimonials block
+  const testimonialsSwiper = new Swiper(".testimonials__slider", {
+    loop: true,
+    slidesPerView: 1,
+    navigation: {
+      nextEl: ".prev",
+      prevEl: ".next",
+    },
+  });
 });
