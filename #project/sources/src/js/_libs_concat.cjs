3b98b2e87443c3dfa5db2e0d804c8932007b4bed
Vasil Ilyuhin <v.ilyuhin@gmail.com>
2022-10-15 16:55:21 +0300

    feat: project complete

--- #project/sources/src/js/_libs_concat.cjs
+++ #project/sources/src/js/_libs_concat.cjs
@@ -1,3 +1,8 @@
-module.exports = [ // comma separated
-    // "./src/libs/jquery/dist/jquery.min.js"
+module.exports = [
+  // comma separated
+  // "./src/libs/jquery/dist/jquery.min.js"
+  //   './src/libs/jquery.ScrollMagic.min.js',
+  //   './src/libs/animation.gsap.min.js',
+  //   './src/libs/animation.velocity.min.js',
+  //   './src/libs/ScrollMagic.min.js',
 ];
