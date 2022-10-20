515ec03858d787e3b3531805adcd5b9a3e5cbb63
Vasily Ilyuhin <v.ilyuhin@gmail.com>
2022-10-20 09:48:41 +0300

    feat: project completed

--- #project/sources/src/js/_libs_concat.cjs
+++ #project/sources/src/js/_libs_concat.cjs
@@ -1,6 +1,7 @@
 module.exports = [
   './src/libs/scrollmagic/scrollmagic/minified/ScrollMagic.min.js',
+  // "./src/libs/scrollmagic/scrollmagic/minified/plugins/debug.addIndicators.min.js",
   './src/libs/scrollmagic/scrollmagic/minified/plugins/animation.gsap.min.js',
   './src/libs/gsap.min.js',
-  './src/libs/swiper-bundle.min.js'
+  './src/libs/swiper-bundle.min.js',
 ];
--- #project/sources/src/js/main.js
+++ #project/sources/src/js/main.js
@@ -1,152 +1,135 @@
-let controller = new ScrollMagic.Controller();
+document.addEventListener("DOMContentLoaded", function (event) {
+  let controller = new ScrollMagic.Controller();
 
-// Определение начального значения трансформации для build__title
-const TL_titleColorChangeWide = gsap.timeline().fromTo(
-  '.build__title',
-  {
-    color: '#0c2742',
-    scale: 1.56,
-    translateX: '-52%',
-    translateY: '125%',
-  },
-  { color: '#fff', scale: 1, translateX: '0', translateY: '0' }
-);
+  // Определение начального значения трансформации для build__title
+  const TL_titleColorChangeWide = gsap.timeline().fromTo(
+    ".build__title",
+    {
+      color: "#0c2742",
+      scale: 1.56,
+      translateX: "-52%",
+      translateY: "125%",
+    },
+    { color: "#fff", scale: 1, translateX: "0", translateY: "0" }
+  );
 
-const TL_titleColorChangeNarrow = gsap.timeline().fromTo(
-  '.build__title',
-  {
-    color: '#0c2742',
-    scale: 1.56,
-    translateX: '24%',
-    translateY: '125%',
-  },
-  { color: '#fff', scale: 1, translateX: '0', translateY: '0' }
-);
+  const TL_titleColorChangeNarrow = gsap.timeline().fromTo(
+    ".build__titleSmall",
+    {
+      color: "#0c2742",
+      scale: 1.2,
+      translateX: "24%",
+      translateY: "125%",
+    },
+    { color: "#fff", scale: 1, translateX: "0", translateY: "0" }
+  );
 
-// Создание сцен для смещения build__title
+  // Создание сцен для смещения build__title
 
-const wideScene = new ScrollMagic.Scene({
-  triggerElement: '.build',
-  triggerHook: 'onLeave',
-  offset: '250%',
-  duration: '25%',
-}).setTween(TL_titleColorChangeWide);
+  const wideScene = new ScrollMagic.Scene({
+    triggerElement: ".build",
+    triggerHook: "onLeave",
+    offset: "250%",
+    duration: "25%",
+  }).setTween(TL_titleColorChangeWide);
 
-const narrowScene = new ScrollMagic.Scene({
-  triggerElement: '.build',
-  triggerHook: 'onLeave',
-  offset: '250%',
-  duration: '25%',
-}).setTween(TL_titleColorChangeNarrow);
+  const narrowScene = new ScrollMagic.Scene({
+    triggerElement: ".build",
+    triggerHook: "onLeave",
+    offset: "250%",
+    duration: "25%",
+  }).setTween(TL_titleColorChangeNarrow);
 
-if (window.innerWidth >= 768) {
   wideScene.addTo(controller);
-} else {
   narrowScene.addTo(controller);
-}
 
-let startWidth = window.innerWidth;
-
-// Изменение смещения при изменении экрана
-window.addEventListener('resize', () => {
-  if (startWidth >= 768 && window.innerWidth < 768) {
-    controller.removeScene(wideScene);
-    narrowScene.addTo(controller);
-    startWidth = 700;
-    console.log('to small');
-  }
-  if (startWidth < 768 && window.innerWidth >= 768) {
-    controller.removeScene(narrowScene);
-    wideScene.addTo(controller);
-    startWidth = 800;
-    console.log('to wide');
-  }
-});
-
-//====================================================
-
-document.addEventListener('DOMContentLoaded', function (event) {
+  //====================================================
   // fix header by scroll
   new ScrollMagic.Scene({
-    triggerElement: '.first',
-    triggerHook: 'onLeave',
-    offset: '20px',
+    triggerElement: ".first",
+    triggerHook: "onLeave",
+    offset: "20px",
     duration: 0,
   })
-    .setClassToggle('.first', 'fixed')
+    .setClassToggle(".first", "fixed")
     .addTo(controller);
 
-  // move about block
-  let moveAboutBlock = gsap.timeline().to('.about__container', { x: '-105%' });
+  const tl = gsap.timeline();
+  tl.fromTo(
+    ".about__container",
+    { x: "0", y: "0" },
+    { x: "-50%", y: "0", duration: 9 }
+  );
 
-  new ScrollMagic.Scene({
-    triggerElement: '.about__container',
-    triggerHook: 'onLeave',
-    duration: '200%',
-  })
-    .setPin('.about__container')
-    .setTween(moveAboutBlock)
-    .addTo(controller);
+  ScrollTrigger.create({
+    animation: tl,
+    trigger: ".about",
+    start: "top top",
+    end: "bottom",
+    scrub: true,
+    pin: true,
+  });
+  //===========================
 
   // move blue figure
   new ScrollMagic.Scene({
-    triggerElement: '.svg-image-pathnew',
-    triggerHook: 'onLeave',
-    offset: '800%',
-    duration: '400%',
+    triggerElement: ".about",
+    triggerHook: "onEnter",
+    offset: "900px",
+    duration: "5000px",
   })
-    .setPin('.svg-image-pathnew')
+    .setPin(".svg-image-pathnew")
     .addTo(controller);
 
   // change scale of blue figure
   const TL_changeScale = gsap
     .timeline()
-    .fromTo('.svg-image-pathnew', { scale: 1 }, { scale: 20 });
+    .fromTo(".svg-image-pathnew", { scale: 1 }, { scale: 20 });
 
   new ScrollMagic.Scene({
-    triggerElement: '.build',
-    triggerHook: 'onLeave',
-    offset: '250%',
-    duration: '25%',
+    triggerElement: ".build",
+    triggerHook: "onLeave",
+    offset: "250%",
+    duration: "25%",
   })
-    .setPin('.svg-image-pathnew')
+    // .setPin('.svg-image-pathnew')
     .setTween(TL_changeScale)
     .addTo(controller);
 
   // change opacity of blue figure
   const TL_changeFillOpacity = gsap
     .timeline()
-    .fromTo('.blue__path', { fillOpacity: 0.06 }, { fillOpacity: 1 });
+    .fromTo(".blue__path", { fillOpacity: 0.06 }, { fillOpacity: 1 });
 
   new ScrollMagic.Scene({
-    triggerElement: '.build',
-    triggerHook: 'onLeave',
-    offset: '250%',
-    duration: '25%',
+    triggerElement: ".build",
+    triggerHook: "onLeave",
+    offset: "250%",
+    duration: "25%",
   })
-    .setPin('.svg-image-pathnew')
+    // .setPin('.svg-image-pathnew')
     .setTween(TL_changeFillOpacity)
     .addTo(controller);
 
   // fix the build block
   new ScrollMagic.Scene({
-    triggerElement: '.build',
-    triggerHook: 'onLeave',
-    duration: '150%',
+    triggerElement: ".build",
+    triggerHook: "onLeave",
+    duration: "900px",
   })
-    .setPin('.build', { pushFollowers: true })
+    .setPin(".build")
     .addTo(controller);
 
   // change opacity of block info
   const TL_changeBuildOpacity = gsap
     .timeline()
-    .fromTo('.build__hidden', { opacity: 0 }, { opacity: 1 });
+    .fromTo(".build__hidden", { opacity: 0 }, { opacity: 1 });
 
   new ScrollMagic.Scene({
-    triggerElement: '.build',
-    triggerHook: 'onLeave',
-    offset: '600%',
-    duration: '40%',
+    triggerElement: ".build",
+    triggerHook: "onLeave",
+    offset: "600%",
+    duration: "40%",
   })
     .setTween(TL_changeBuildOpacity)
     .addTo(controller);
@@ -154,13 +137,13 @@ document.addEventListener('DOMContentLoaded', function (event) {
   // change opacity of block info
   const TL_translateBuildMore = gsap
     .timeline()
-    .fromTo('.build__more', { opacity: 0, y: '50px' }, { opacity: 1, y: '0' });
+    .fromTo(".build__more", { opacity: 0, y: "50px" }, { opacity: 1, y: "0" });
 
   new ScrollMagic.Scene({
-    triggerElement: '.build',
-    triggerHook: 'onLeave',
-    offset: '650%',
-    duration: '10%',
+    triggerElement: ".build",
+    triggerHook: "onLeave",
+    offset: "650%",
+    duration: "10%",
   })
     .setTween(TL_translateBuildMore)
     .addTo(controller);
@@ -168,12 +151,12 @@ document.addEventListener('DOMContentLoaded', function (event) {
   // .news-pathnew translate
   const TL_newFigureTransform = gsap
     .timeline()
-    .fromTo('.news-pathnew', { translateY: '40%' }, { translateY: '-23%' });
+    .fromTo(".news-pathnew", { translateY: "40%" }, { translateY: "-23%" });
 
   new ScrollMagic.Scene({
-    triggerElement: '.news',
-    triggerHook: 'onLeave',
-    duration: '70%',
+    triggerElement: ".news",
+    triggerHook: "onLeave",
+    duration: "70%",
   })
     .setTween(TL_newFigureTransform)
     .addTo(controller);
@@ -182,21 +165,34 @@ document.addEventListener('DOMContentLoaded', function (event) {
   const TL_newPathChangeColor = gsap
     .timeline()
     .fromTo(
-      '.news__path',
-      { fill: '#1B75BC', fillOpacity: '0.06' },
-      { fill: '#02AD23', fillOpacity: '1' }
+      ".news__path",
+      { fill: "#1B75BC", fillOpacity: "0.06" },
+      { fill: "#02AD23", fillOpacity: "1" }
     );
 
   new ScrollMagic.Scene({
-    triggerElement: '.news',
-    triggerHook: 'onLeave',
-    duration: '70%',
+    triggerElement: ".news",
+    triggerHook: "onLeave",
+    duration: "70%",
   })
     .setTween(TL_newPathChangeColor)
     .addTo(controller);
 
+  // .testimonials__infowrapper change color
+  const TL_infowrapper = gsap
+    .timeline()
+    .fromTo(".testimonials__infowrapper", { opacity: "0" }, { opacity: "1" });
+
+  new ScrollMagic.Scene({
+    triggerElement: ".news",
+    triggerHook: "onLeave",
+    duration: "70%",
+  })
+    .setTween(TL_infowrapper)
+    .addTo(controller);
+
   // Slider in news block
-  const newsSwiper = new Swiper('.news__slider', {
+  const newsSwiper = new Swiper(".news__slider", {
     loop: true,
     slidesPerView: 1,
     spaceBetween: 50,
@@ -219,66 +215,60 @@ document.addEventListener('DOMContentLoaded', function (event) {
     },
   });
 
-  document
-    .querySelector('.testimonials__prev')
-    .addEventListener('click', () => {
-      console.log('prev');
-    });
-
   // Slider in testimonials block
-  const testimonialsSwiper = new Swiper('.testimonials__slider', {
+  const testimonialsSwiper = new Swiper(".testimonials__slider", {
     loop: true,
     slidesPerView: 1,
     navigation: {
-      nextEl: '.prev',
-      prevEl: '.next',
+      nextEl: ".next",
+      prevEl: ".prev",
     },
   });
 
   // map
   const body = document.body;
-  const mapOpener = document.querySelector('.footer__map');
-  const map = document.querySelector('.map');
-  const mapOverlay = document.querySelector('.map__overlay');
-  const mapClose = document.querySelector('.map__close');
+  const mapOpener = document.querySelector(".footer__map");
+  const map = document.querySelector(".map");
+  const mapOverlay = document.querySelector(".map__overlay");
+  const mapClose = document.querySelector(".map__close");
 
-  mapOpener.addEventListener('click', () => {
-    map.classList.add('active');
-    body.classList.add('lock');
+  mapOpener.addEventListener("click", () => {
+    map.classList.add("active");
+    body.classList.add("lock");
   });
 
-  mapOverlay.addEventListener('click', () => {
-    map.classList.remove('active');
-    body.classList.remove('lock');
+  mapOverlay.addEventListener("click", () => {
+    map.classList.remove("active");
+    body.classList.remove("lock");
   });
 
-  mapClose.addEventListener('click', () => {
-    map.classList.remove('active');
-    body.classList.remove('lock');
+  mapClose.addEventListener("click", () => {
+    map.classList.remove("active");
+    body.classList.remove("lock");
   });
 
   // burger-menu
-  const menuBurger = document.querySelector('.menu__burger');
-  const burgerNavbar = document.querySelector('.burger__navbar');
-  const burgerOverlay = document.querySelector('.burger__overlay');
-  const burgerLinks = document.querySelectorAll('.burger__link');
-  const burgerApply = document.querySelector('.burger__appply');
+  const menuBurger = document.querySelector(".menu__burger");
+  const burgerNavbar = document.querySelector(".burger__navbar");
+  const burgerOverlay = document.querySelector(".burger__overlay");
+  const burgerLinks = document.querySelectorAll(".burger__link");
+  const burgerApply = document.querySelector(".burger__appply");
 
-  menuBurger.addEventListener('click', () => {
-    menuBurger.classList.toggle('active');
-    burgerNavbar.classList.toggle('active');
-    burgerOverlay.classList.toggle('active');
-    body.classList.toggle('lock');
+  menuBurger.addEventListener("click", () => {
+    menuBurger.classList.toggle("active");
+    burgerNavbar.classList.toggle("active");
+    burgerOverlay.classList.toggle("active");
+    body.classList.toggle("lock");
   });
 
   const hideBurgerNavbar = () => {
-    menuBurger.classList.remove('active');
-    burgerNavbar.classList.remove('active');
-    burgerOverlay.classList.remove('active');
-    body.classList.remove('lock');
+    menuBurger.classList.remove("active");
+    burgerNavbar.classList.remove("active");
+    burgerOverlay.classList.remove("active");
+    body.classList.remove("lock");
   };
 
-  burgerOverlay.addEventListener('click', hideBurgerNavbar);
-  burgerApply.addEventListener('click', hideBurgerNavbar);
-  burgerLinks.forEach((el) => el.addEventListener('click', hideBurgerNavbar));
+  burgerOverlay.addEventListener("click", hideBurgerNavbar);
+  burgerApply.addEventListener("click", hideBurgerNavbar);
+  burgerLinks.forEach((el) => el.addEventListener("click", hideBurgerNavbar));
 });
